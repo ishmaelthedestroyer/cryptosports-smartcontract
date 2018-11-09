b942d0e01da634882599a72160def9ffaacfb63b
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2018-11-08 22:52:18 -0800

    migration

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = 'slide faith cradle suffer more toilet monkey squeeze pride april fault will';
+const infuraURL = 'https://ropsten.infura.io/Cvr4R20I8eWayqlooYwR';
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -8,9 +12,8 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-      host: "localhost",
-      port: 8545,
-      network_id: "3"
+      provider: new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 3
     }
   }
 };
