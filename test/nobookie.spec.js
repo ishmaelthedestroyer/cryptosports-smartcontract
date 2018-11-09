c4ee52f35f9bba3f0b65488fe3373bc9a220715d
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2018-11-08 22:25:04 -0800

    disburse bet minus fees

--- test/nobookie.spec.js
+++ test/nobookie.spec.js
@@ -1,6 +1,11 @@
 const NoBookie = artifacts.require("./NoBookie");
 
 contract('NoBookie', function(accounts) {
+  const amount = 1;
+  const feePercent = 0.03;
+  const fee = amount * feePercent;
+  const value = web3.toWei(amount, 'ether');
+
   it("Initial NoBookie settings should match", async () => {
     let
       nobookie = await NoBookie.deployed(),
@@ -12,13 +17,13 @@ contract('NoBookie', function(accounts) {
   it("Should be able to create and accept a bet", async () => {
     let
       nobookie = await NoBookie.deployed(),
-      addStatus = await nobookie.addBet.call('123', {
+      addStatus = await nobookie.addBet('123', {
         from: accounts[1],
-        value: 100,
+        value
       }),
-      acceptStatus = await nobookie.acceptBet.call('123', {
+      acceptStatus = await nobookie.acceptBet('123', {
         from: accounts[2],
-        value: 100,
+        value
       });
 
     assert(addStatus);
@@ -27,17 +32,17 @@ contract('NoBookie', function(accounts) {
   it("Should be able to disburse winnings", async () => {
     let
       nobookie = await NoBookie.deployed(),
-      addStatus = await nobookie.addBet.call('456', {
+      addStatus = await nobookie.addBet('456', {
         from: accounts[1],
-        value: 100,
+        value
       }),
-      acceptStatus = await nobookie.acceptBet.call('456', {
+      acceptStatus = await nobookie.acceptBet('456', {
         from: accounts[2],
-        value: 100,
+        value
+      }),
+      disburseStatus = await nobookie.disburseBet.call('123', accounts[2], fee, {
+        from: accounts[0],
       });
-      // disburseStatus = await nobookie.disburseBet.call('123', accounts[2], {
-      //   from: accounts[0],
-      // });
 
     assert(addStatus && acceptStatus && disburseStatus);
   });
